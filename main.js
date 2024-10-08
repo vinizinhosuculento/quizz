dd9eb498a30566617919482dba4e6b6eea391fbf
Vinícius <147394792+vinizinhosuculento@users.noreply.github.com>
2024-10-08 08:50:41 -0300

    x

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ let cartao= createElement (article)
 cartao.className="cartao"
 console.log (cartao)
 }
+cartao.innerHTML=
+<div class="cartao--conteudo">"
+                    <div class="cartao-pergunta">
+                        <p>Qual é o país mais populoso do mundo?</p>
+                    </div>
+                        <div class="cartao-respota">
+                            <p>A India</p>
+                        </div>
+                        "
+                        console.log
